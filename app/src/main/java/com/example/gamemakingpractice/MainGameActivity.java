94f5b082cf41dc962291ff15e89faad9c998bd26
DavidRuff06 <90778233+DavidRuff06@users.noreply.github.com>
2022-10-28 12:50:02 -0500

    Blank buttonClick added

--- app/src/main/java/com/example/gamemakingpractice/MainGameActivity.java
+++ app/src/main/java/com/example/gamemakingpractice/MainGameActivity.java
@@ -3,14 +3,17 @@ package com.example.gamemakingpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainGameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.acitivty_main_game);
     }
 
-    public void 
+    public void buttonClick(View v){
+        
+    }
 }
