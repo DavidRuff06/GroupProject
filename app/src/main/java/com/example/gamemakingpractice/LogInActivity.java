1e8e479724a8ee6f5028d96000f880ef96aceb3d
abhi0516 <90794084+abhi0516@users.noreply.github.com>
2022-10-28 12:54:38 -0500

    Please work

--- app/src/main/java/com/example/gamemakingpractice/LogInActivity.java
+++ app/src/main/java/com/example/gamemakingpractice/LogInActivity.java
@@ -3,7 +3,6 @@ package com.example.gamemakingpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-<<<<<<< Updated upstream
 import android.view.View;
 
 <<<<<<< Updated upstream:app/src/main/java/com/example/gamemakingpractice/MainGameActivity.java
@@ -11,15 +10,10 @@ public class MainGameActivity extends AppCompatActivity {
 =======
 public class LogInActivity extends AppCompatActivity {
 >>>>>>> Stashed changes:app/src/main/java/com/example/gamemakingpractice/LogInActivity.java
-=======
-
-public class MainGameActivity extends AppCompatActivity {
->>>>>>> Stashed changes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< Updated upstream
 <<<<<<< Updated upstream:app/src/main/java/com/example/gamemakingpractice/MainGameActivity.java
         setContentView(R.layout.acitivty_main_game);
     }
@@ -31,10 +25,4 @@ public class MainGameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_log_in);
     }
 >>>>>>> Stashed changes:app/src/main/java/com/example/gamemakingpractice/LogInActivity.java
-=======
-        setContentView(R.layout.activity_main_game);
-    }
-
-    public void
->>>>>>> Stashed changes
 }
