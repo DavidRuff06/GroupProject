bf6e71aedc234a424a5a711c3d206218d3fe17b9
Golsuc <90780174+Golsuc@users.noreply.github.com>
2022-11-04 12:54:38 -0500

    Still working on Firebase

--- app/src/main/java/com/example/groupproject/MainGameActivity.java
+++ app/src/main/java/com/example/groupproject/MainGameActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainGameActivity extends AppCompatActivity {
     int cryptoCount;
+    public final String TAG = "Group";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,13 @@ public class MainGameActivity extends AppCompatActivity {
     }
     public void cryptoSelectorClicked(View view){
         Intent intent = new Intent(this, CryptoSelectorActivity.class);
+        startActivity(intent);
+    }
+
+    public void logOutClicked(View view) {
+        SignInActivity.firebaseHelper.logOutUser();
+        Log.i(TAG, "user logged out");
+        Intent intent = new Intent(MainGameActivity.this, SignInActivity.class);
         startActivity(intent);
     }
 }
