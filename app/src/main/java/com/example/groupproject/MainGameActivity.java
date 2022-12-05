06b75837da0daf47ab9ca50832563a29280d88ca
DavidRuff06 <90778233+DavidRuff06@users.noreply.github.com>
2022-12-05 12:08:41 -0600

    Removing unneeded api usage

--- app/src/main/java/com/example/groupproject/MainGameActivity.java
+++ app/src/main/java/com/example/groupproject/MainGameActivity.java
@@ -49,7 +49,7 @@ public class MainGameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_game_activity);
         currencyModalArrayList = new ArrayList<>();
-       getData();
+       //getData();
         prefs = getSharedPreferences("com.example.groupproject", MODE_PRIVATE);
     }
 
@@ -131,73 +131,73 @@ public class MainGameActivity extends AppCompatActivity {
     }
 
 
-    private void getData() {
-        // creating a variable for storing our string.
-        String url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
-        // creating a variable for request queue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        // making a json object request to fetch data from API.
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                // inside on response method extracting data
-                // from response and passing it to array list
-                // on below line we are making our progress
-                // bar visibility to gone.
-                try {
-                    // extracting data from json.
-                    JSONArray dataArray = response.getJSONArray("data");
-                    for (int i = 0; i < dataArray.length(); i++) {
-                        JSONObject dataObj = dataArray.getJSONObject(i);
-                        String symbol = dataObj.getString("symbol");
-                        String name = dataObj.getString("name");
-                        JSONObject quote = dataObj.getJSONObject("quote");
-                        JSONObject USD = quote.getJSONObject("USD");
-                        double price = USD.getDouble("price");
-                        // adding all data to our array list.
-                        if (name.equals("Bitcoin")) {
-                            currencyModalArrayList.add(0, new CurrencyModal(name, symbol, price));
-                        }
-                        if (name.equals("Dogecoin")) {
-                            currencyModalArrayList.add(1, new CurrencyModal(name, symbol, price));
-                        }
-                    }
-                    // notifying adapter on data change.
-                } catch (JSONException e) {
-                    // handling json exception.
-                    e.printStackTrace();
-                    Toast.makeText(MainGameActivity.this, "Something went amiss. Please try again later", Toast.LENGTH_SHORT).show();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                // displaying error response when received any error.
-                Toast.makeText(MainGameActivity.this, "Something went amiss. Please try again later", Toast.LENGTH_SHORT).show();
-            }
-        }) {
-            @Override
-            public Map<String, String> getHeaders() {
-                // in this method passing headers as
-                // key along with value as API keys.
-                HashMap<String, String> headers = new HashMap<>();
-                headers.put("X-CMC_PRO_API_KEY", "1cae5f1b-5842-4ad9-8920-69ed8bb95f58");
-                // at last returning headers
-                return headers;
-            }
-        };
-        // calling a method to add our
-        // json object request to our queue.
-        queue.add(jsonObjectRequest);
-    }
+//    private void getData() {
+//        // creating a variable for storing our string.
+//        String url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+//        // creating a variable for request queue.
+//        RequestQueue queue = Volley.newRequestQueue(this);
+//        // making a json object request to fetch data from API.
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject response) {
+//                // inside on response method extracting data
+//                // from response and passing it to array list
+//                // on below line we are making our progress
+//                // bar visibility to gone.
+//                try {
+//                    // extracting data from json.
+//                    JSONArray dataArray = response.getJSONArray("data");
+//                    for (int i = 0; i < dataArray.length(); i++) {
+//                        JSONObject dataObj = dataArray.getJSONObject(i);
+//                        String symbol = dataObj.getString("symbol");
+//                        String name = dataObj.getString("name");
+//                        JSONObject quote = dataObj.getJSONObject("quote");
+//                        JSONObject USD = quote.getJSONObject("USD");
+//                        double price = USD.getDouble("price");
+//                        // adding all data to our array list.
+//                        if (name.equals("Bitcoin")) {
+//                            currencyModalArrayList.add(0, new CurrencyModal(name, symbol, price));
+//                        }
+//                        if (name.equals("Dogecoin")) {
+//                            currencyModalArrayList.add(1, new CurrencyModal(name, symbol, price));
+//                        }
+//                    }
+//                    // notifying adapter on data change.
+//                } catch (JSONException e) {
+//                    // handling json exception.
+//                    e.printStackTrace();
+//                    Toast.makeText(MainGameActivity.this, "Something went amiss. Please try again later", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                // displaying error response when received any error.
+//                Toast.makeText(MainGameActivity.this, "Something went amiss. Please try again later", Toast.LENGTH_SHORT).show();
+//            }
+//        }) {
+//            @Override
+//            public Map<String, String> getHeaders() {
+//                // in this method passing headers as
+//                // key along with value as API keys.
+//                HashMap<String, String> headers = new HashMap<>();
+//                headers.put("X-CMC_PRO_API_KEY", "1cae5f1b-5842-4ad9-8920-69ed8bb95f58");
+//                // at last returning headers
+//                return headers;
+//            }
+//        };
+//        // calling a method to add our
+//        // json object request to our queue.
+//        queue.add(jsonObjectRequest);
+//    }
 
-    public static ArrayList<CurrencyModal> getCurrencyModalArrayList() {
-        return currencyModalArrayList;
-    }
-
-    public static void setCurrencyModalArrayList(ArrayList<CurrencyModal> currencyModalArrayList) {
-        MainGameActivity.currencyModalArrayList = currencyModalArrayList;
-    }
+//    public static ArrayList<CurrencyModal> getCurrencyModalArrayList() {
+//        return currencyModalArrayList;
+//    }
+//
+//    public static void setCurrencyModalArrayList(ArrayList<CurrencyModal> currencyModalArrayList) {
+//        MainGameActivity.currencyModalArrayList = currencyModalArrayList;
+//    }
     public static double getBitcoinPrice() {
         CurrencyModal bitcoin = currencyModalArrayList.get(0);
         return bitcoin.getPrice();
